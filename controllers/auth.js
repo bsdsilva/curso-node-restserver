1cdee80214e7abffd7fbec5931081a830e1baa35
Barbara da Silva <barbara@hazling.com>
2024-02-02 14:39:05 -0400

    Fin seccion 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req, res = response) => {
 
@@ -42,6 +43,52 @@ const login = async(req, res = response) => {
     }
 }
 
+const googleSignIn = async(req, res=response) => {
+    
+    const {id_token} = req.body;
+
+    try{
+
+        const { nombre, img, correo} = await googleVerify(id_token);
+        
+        let usuario = await Usuario.findOne({correo});
+        if(!usuario){
+            //Crearlo
+            const data = {
+                nombre, 
+                correo, 
+                password: ':p',
+                google: true,
+                rol: 'USER_ROLE'
+            };
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //Si el usuario en BD tiene estado=false
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        //Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario, token
+        });
+
+    }catch(error){
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
